f60b78f88f0463f16f11ef70d3e86de7a22c92e4
marceloSantosBrl <conteudospam@outlook.com>
2022-12-13 18:41:30 -0300

    Atividade da semana 03

--- M1S03/Ex06/app.js
+++ M1S03/Ex06/app.js
@@ -1,4 +1,5 @@
 const verificaPalindromo = (str) => {
+  if (str === null || str.length === 0) return false;
   for (let i = 0; i < str.length / 2 + 1; i += 1) {
     if (str[i] !== str[str.length - 1 - i]) return false;
   }
