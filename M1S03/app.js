cdb13405022215682be65ad9b87c8bb75e8cd74d
marceloSantosBrl <conteudospam@outlook.com>
2022-12-12 21:13:12 -0300

    Atividade da semana 03

--- M1S03/app.js
+++ M1S03/app.js
@@ -23,4 +23,12 @@ const procuraMinMax = (arr) => {
   }
 };
 
+const mensagemOla = (name) => `Olá, ${name}`;
+
+const writeMessage = () => {
+  const input = document.querySelector('#name');
+  const text = document.querySelector('.text');
+  text.innerText = mensagemOla(input.value);
+};
+
 module.exports = minMax;
