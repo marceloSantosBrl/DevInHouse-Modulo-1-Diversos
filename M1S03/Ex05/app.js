c8eb8e1522d68178f4cdd11d917a2da9ae96631f
marceloSantosBrl <conteudospam@outlook.com>
2022-12-13 16:42:19 -0300

    Atividade da semana 03

--- M1S03/Ex05/app.js
+++ M1S03/Ex05/app.js
@@ -1,4 +1,4 @@
-const somaTudo = (...arr) => {
-  const initialValue = 0;
-  return arr.reduce((accumulator, current) => accumulator + current, initialValue);
-};
+const somaTudo = (...arr) => arr.reduce((accumulator, current) => accumulator + current, 0);
+
+const resultado = somaTudo(1, 2, 3, 4);
+console.log(resultado);
