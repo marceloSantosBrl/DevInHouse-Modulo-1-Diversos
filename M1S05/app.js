4821e5ad15aa2426a1805d9a38b45aa58653724f
Marcelo Santos <marcelegald2@outlook.com>
2023-02-02 19:20:45 -0300

    [M1S05] Ex 6 - Classe Juros

--- M1S05/app.js
+++ M1S05/app.js
@@ -3,6 +3,7 @@ import Fatura from './Fatura.js';
 import Personagem from './Personagem.js';
 import { Cachorro, Gato } from './Animal.js';
 import CalculadoraDeArea from './CalculadoraDeArea.js';
+import Juros from './Juros.js';
 
 let marcelo;
 try {
@@ -12,15 +13,13 @@ try {
   console.log(e.message);
 }
 
-
-const melao = new Fatura(123, "Melão", 2, 3);
+const melao = new Fatura(123, 'Melão', 2, 3);
 
 const valor = melao.obterValorTotal();
 
 console.log(valor); // 6
 
-
-const alyx = new Personagem("Alyx");
+const alyx = new Personagem('Alyx');
 
 console.log(alyx.percentualVida); // 100
 
@@ -32,20 +31,27 @@ alyx.usouKitMedico();
 
 console.log(alyx.percentualVida); // 60
 
-
 const snoopy = new Cachorro('snoopy', 10);
-console.log( snoopy.som );   // "latido"     
+console.log(snoopy.som); // "latido"
 const frajola = new Gato('frajola', 8);
-console.log( frajola.som );   // "miado"
+console.log(frajola.som); // "miado"
 
-const triangB = new CalculadoraDeArea("triangulo", 6, 8);
+const triangB = new CalculadoraDeArea('triangulo', 6, 8);
 const areaTB = triangB.calcular();
 console.log(areaTB);
 
-const quadA = new CalculadoraDeArea("quadrado", 4);
+const quadA = new CalculadoraDeArea('quadrado', 4);
 const areaQA = quadA.calcular();
 console.log(areaQA);
 
-const retA = new CalculadoraDeArea("retangulo", 4, 6);
+const retA = new CalculadoraDeArea('retangulo', 4, 6);
 const areaRT = retA.calcular();
 console.log(areaRT);
+
+const testeJuros1 = new Juros(10000, 0.07, 24);
+const testeJuros2 = new Juros(10000, 0.15, 10);
+
+console.log(`Juros Simples testeJuros1: ${testeJuros1.calcularJurosSimples()}, 
+Juros composto testeJuros1: ${testeJuros1.calcularJurosComposto()}`);
+console.log(`Juros Simples testeJuros2: ${testeJuros2.calcularJurosSimples()},
+Juros composto testeJuros2: ${testeJuros2.calcularJurosComposto()}`);
